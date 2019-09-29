53a2e84109ef27b1d497174e6ad63b2f9991951b
Giovanni Armane <giovanniarmane@gmail.com>
2019-09-29 14:11:55 -0300

    Define endpoints for form registration of new user

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,9 +4,27 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Repo
   alias Rumbl.User
 
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+    {:ok, user} = Repo.insert(changeset)
+  end
+
   def index(conn, _params) do
     users = Repo.all User
     render conn, "index.html", users: users
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 
   def show(conn, %{"id" => id}) do
