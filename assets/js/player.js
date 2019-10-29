7484a4119aaf2130b033552c9877d771946bc069
Giovanni Armane <giovanniarmane@gmail.com>
2019-10-29 17:52:10 -0300

    Define client-side socket connection

--- assets/js/player.js
+++ assets/js/player.js
@@ -5,19 +5,19 @@ const player = {
     window.onYouTubeIframeAPIReady = () => {
       this.onIframeReady(domId, playerId, onReady);
     }
-    let youtubeScriptTag = document.createElement("script");
-    youtubeScriptTag.src = "//www.youtube.com/iframe_api";
+    const youtubeScriptTag = document.createElement('script');
+    youtubeScriptTag.src = '//www.youtube.com/iframe_api';
     document.head.appendChild(youtubeScriptTag)
   },
 
   onIframeReady(domId, playerId, onReady) {
     this.player = new YT.Player(domId, {
-      height: "360",
-      width: "420",
+      height: '360',
+      width: '420',
       videoId: playerId,
       events: {
-        "onReady": (event => onReady(event)),
-        "onStateChange": (event => this.onPlayerStateChange(event))
+        'onReady': (event => onReady(event)),
+        'onStateChange': (event => this.onPlayerStateChange(event))
       }
     });
   },
